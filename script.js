aa3ec5b1b7d11024dcd01a2dd470515b73b62116
Shrikanttathe-hub <142688445+Shrikanttathe-hub@users.noreply.github.com>
2024-02-12 21:51:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 //complete this code
 class Rectangle {
 	constructor(width, height){
-		this.width = width;
-		this.height = height;
+		this._width = width;
+		this._height = height;
 		
 	}
      get width(){
-		 return this.width;
+		 return this._width;
 	 }
 	 get height(){
-		 return this.height;
+		 return this._height;
 	 }
 	getArea(){
-		return this.width * this.height;
+		return this._width * this._height;
 	}
 }
 class Square extends Rectangle{
@@ -20,7 +20,7 @@ class Square extends Rectangle{
 		super(side,side);
 	}
 	getPerimeter(){
-		return 4*this.width;
+		return 4 * this.width;
 	}
 }
 
@@ -31,6 +31,12 @@ const rectangle = new Rectangle(5, 10);
 console.log(rectangle.width); // Output: 5
 console.log(rectangle.height); // Output: 10
 console.log(rectangle.getArea()); // Output: 50
+
+const square = new Square(7);
+console.log(square.width); // Output: 7
+console.log(square.height); // Output: 7
+console.log(square.getArea()); // Output: 49
+console.log(square.getPerimeter()); // Output: 28
 
 window.Rectangle = Rectangle;
 window.Square = Square;
